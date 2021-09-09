669cbb1d8a584e9ccbfd048dbfbfbb3d9457d81e
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-09-09 13:21:29 -0400

    added signout option on header via auth converted app to class based component to be able to detect if the user is logged in via component will mount

--- src/App.js
+++ src/App.js
@@ -5,24 +5,53 @@ import HomePage from "./pages/homepage/homePage.Component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+import {auth} from "./components/firebase/firebase.utils";
 
 
-const App = () => (
-    <div>
-        <Header/>
-        <Switch>
+class App extends React.Component {
 
-            <Route exact path='/' component={HomePage}/>
-            {/*
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: null
+
+        }
+    }
+
+    unsubscribeFromAUth = null;
+
+    componentDidMount() {
+        auth.onAuthStateChanged(user => {
+            // auth will change user will be updated here.  user is the parameter for the user object.
+            this.setState({currentUser: user})
+            console.log(user)
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAUth();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header currentUser={this.state.currentUser}/>
+                <Switch>
+
+                    <Route exact path='/' component={HomePage}/>
+                    {/*
         /*
         exact is what the exact path will be the path has to be exactly for it to work.
         path --> how will we get to page
         component --> where will the page will go.
         */}
-        <Route path='/shop' component={ShopPage}/>
-        <Route path='/contact' component={SignInSignUpPage}/>
-        </Switch>
-    </div>
-);
+                    <Route path='/shop' component={ShopPage}/>
+                    <Route path='/contact' component={SignInSignUpPage}/>
+                </Switch>
+            </div>
+        );
+    }
+}
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './header.styles.scss'
 import {Link} from "react-router-dom";
 import {ReactComponent as Logo} from "../../assests/crown.svg";
+import {auth} from "../firebase/firebase.utils";
 
-const Header = () => {
+const Header = ({currentUser}) => {
     return (
         <div className="header">
             <Link to='/' className='logo-container'>
@@ -12,6 +13,14 @@ const Header = () => {
             <div className='options'>
                 <Link className='option' to='/shop'>SHOP</Link>
                 <Link className='option' to='/contact'>Contact</Link>
+                {
+                    currentUser ?
+                        <div className='option' onClick={() => auth.signOut()}>
+                            Sign Out
+                        </div>
+
+                        : <Link></Link>
+                }
             </div>
         </div>
     );
