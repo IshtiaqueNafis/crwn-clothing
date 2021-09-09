14e743f41bc3d3168e944ed3545f21047ad61790
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-09-09 13:31:51 -0400

    added styling for custom button to differciate between google log in and normal email log out

--- src/components/custom-button/custom-Button.componenet.jsx
+++ src/components/custom-button/custom-Button.componenet.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './custom-button.styles.scss'
 
-const CustomButton = ({children, ...otherProps}) => {
+const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => {
     return (
-        <button className='custom-button'
+        <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
                 {...otherProps} // rember the other props has to be passed here for any other property to work.
         >
 
