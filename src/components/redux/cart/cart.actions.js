6432f456e26fa4dbec1afc2d42e4d7ec0115ab2c
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-09-12 13:28:48 -0400

    additem action added on cartReducer

--- src/components/redux/cart/cart.actions.js
+++ src/components/redux/cart/cart.actions.js
@@ -5,3 +5,8 @@ export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
     // note that there is no payload here cause its not using the payload.
 })
+
+export const addItem = (item) => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+})
--- src/components/redux/cart/cart.reducer.js
+++ src/components/redux/cart/cart.reducer.js
@@ -2,7 +2,8 @@ import {CartActionTypes} from "./cart.types";
 
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 const CartReducer = (state = INITIAL_STATE, action) => {
@@ -11,6 +12,11 @@ const CartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden // if its true convert to false if its false convert to true.
+            }
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: [...state.cartItems, action.payload], //... state.cartItems mean spreading the array value for ...state. and action.payload will come after.
             }
         default:
             return state;
